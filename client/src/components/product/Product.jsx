4d30fc892a0ead3390bbdb7f5a7621bdb9247bf7
Toni Tralice <atralice@gmail.com>
2020-05-20 10:26:55 -0300

    changes arrow function for function expression

--- client/src/components/product/Product.jsx
+++ client/src/components/product/Product.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 
-export default ({title, price}) => (
-  <div>
-    <h1>{title}</h1>
-    <p>{price}</p>
-  </div>
-)
+export default function Producto({title, price}) {
+  return (
+    <div>
+      <h1>{title}</h1>
+      <p>{price}</p>
+    </div>
+  );
+}
