c8603f8644b42888ab4a08d071bb99a8c0b85629
Nyarlat <boris.bdc@gmail.com>
2020-05-27 00:37:27 -0300

    52 se agrego la ruta y el controller para Paginación, se modifico el api/index para tener un control mas claro del puerto

--- api/index.js
+++ api/index.js
@@ -22,8 +22,10 @@ const server = require('./src/app.js');
 const { conn } = require('./src/models/index.js');
 
 // Syncing all the models at once.
+const port = 4000;
+
 conn.sync({ force: false }).then(() => {
-  server.listen(3000, () => {
-    console.log('%s listening at 3000'); // eslint-disable-line no-console
+  server.listen(port, () => {
+    console.log(`%s listening at ${port}`); // eslint-disable-line no-console
   });
 });
--- api/src/controllers/productController.js
+++ api/src/controllers/productController.js
@@ -19,6 +19,27 @@ function getOneProduct(req, res, next) {
     .catch(next);
 }
 
+function getProductsPaged(req, res, next){
+  //Define la offset("compensador") y el limite en 0 y 9 respectivamente.
+  let offset = 0;
+  let limit = 10;
+
+  Product.findAndCountAll()
+  .then((data) => {
+    let page = req.params.page;
+    //findAndCountAll crea 2 instancias count(entero - cantidad de coincidencias con la query) y row(array - coincidencias obtenidas)
+    let pages = Math.ceil(data.count / limit)
+    offset = limit * (page - 1)
+
+    Product.findAll({
+      limit,
+      offset,
+      $sort: { id: 1}
+    }).then(result => res.status(201).json(result))
+  }).catch(next);
+
+}
+
 function createProduct(req, res, next) {
   // Si el usuario esta registrado y crea el producto me traigo estos datos
   const { title, description, price, stock, picture } = req.body;
@@ -64,6 +85,7 @@ function deleteProduct(req, res, next) {
 module.exports = {
   getProducts,
   getOneProduct,
+  getProductsPaged,
   createProduct,
   editProduct,
   deleteProduct
