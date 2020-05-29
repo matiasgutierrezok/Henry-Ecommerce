2b66b2df862f7a489c99f9d3ce3bcdc164a4dae4
Sofia Praderio <sdpraderio@live.com>
2020-05-29 12:03:55 -0300

    models add

---
+++ api/src/models/Cart_Item.js
@@ -0,0 +1,19 @@
+const Cart_Item = (db, Sequelize) => {
+	const C = db.define('cart_items', {
+		id: {
+			type: Sequelize.INTEGER,
+			autoIncrement: true,
+			primaryKey: true
+		},
+		quantity: {
+			type: Sequelize.INTEGER,
+			allowNull: false,
+			validate: {
+				min: 0
+			}
+		}
+	});
+	return C;
+}
+
+module.exports = Cart_Item;
